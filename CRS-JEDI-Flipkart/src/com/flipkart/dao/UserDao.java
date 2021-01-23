1e477966e8deb5ed04a0253484743c0ccb9afac4
abhishreejain <45056175+abhishreejain@users.noreply.github.com>
2021-01-24 02:03:14 +0530

    comments added

--- CRS-JEDI-Flipkart/src/com/flipkart/dao/UserDao.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/dao/UserDao.java
@@ -1,25 +1,50 @@
 package com.flipkart.dao;
 
+import java.util.List;
+
 import com.flipkart.bean.User;
 import com.flipkart.constant.Role;
 
 public interface UserDao {
 	
-	// Add User
+	/**
+	 * Add User
+	 * @param user
+	 * @returnType void
+	 */
 	public void addUser(User user);
 	
-	// Delete User
+	/**
+	 * Delete user from list of existing users
+	 * @param userId
+	 * @returnType void
+	 */
 	public void deleteUser(String userId);
 	
-	// Modify User
+	/**
+	 * Modify details of a user
+	 * @param userId
+	 * @returnType void
+	 */
 	public void modifyUser(String userId);
 	
-	// Get all users
-	public void getUsers();
+	/**
+	 * Get a list of ids of all users in the database
+	 * @returnType List<String>
+	 */
+	public List<String> getUsers();
 	
-	// Get all users with particular role
-	public void getUsers(Role role);
+	/**
+	 * Get users with a particular role(Student, professor or admin)
+	 * @param role
+	 * @returnType List<String>
+	 */
+	public List<String> getUsers(Role role);
 	
-	// Get User
+	/**
+	 * Get all details of a particular user
+	 * @param userId
+	 * @returnType User
+	 */
 	public User getUser(String userId);
 }
