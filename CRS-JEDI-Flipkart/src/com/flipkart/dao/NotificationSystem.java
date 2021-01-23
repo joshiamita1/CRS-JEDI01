571faecc530e3e6e71952fc5b1f796005156b447
abhishreejain <45056175+abhishreejain@users.noreply.github.com>
2021-01-24 01:48:25 +0530

    comments added

--- CRS-JEDI-Flipkart/src/com/flipkart/dao/NotificationSystem.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/dao/NotificationSystem.java
@@ -2,5 +2,11 @@ package com.flipkart.dao;
 
 public interface NotificationSystem {
 
+	/**
+	 * Notify user with a given message
+	 * @param userID
+	 * @param Message
+	 * @returnType void
+	 */
 	public void notifyUser(int userID, String Message);
 }
