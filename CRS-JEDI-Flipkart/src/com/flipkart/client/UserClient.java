c30d980e2f1b19d9da9999b31b3bb15efce42142
abhishreejain <45056175+abhishreejain@users.noreply.github.com>
2021-01-25 11:38:18 +0530

    comments

--- CRS-JEDI-Flipkart/src/com/flipkart/client/UserClient.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/client/UserClient.java
@@ -34,12 +34,13 @@ public class UserClient {
 	
 	// Enter Point of the Application
 	public static void main(String[]args) {
+		logger.info("================COURSE REGISTRATION SYSTEM================");
 		logger.info("Welcome to the Course Registration System");
 		
 		// Displaying Login Portal
 		showUserMenu();
 		
-		logger.info("Exiting CRS");		
+		logger.info("========================EXITING CRS=======================");		
 	}
 	
 	// Display Login Portal
@@ -47,10 +48,11 @@ public class UserClient {
 		boolean showMenu=true;
 		int choice;
 		do {
-			logger.info("======User Menu=====");
+			logger.info("===========================USER===========================");
 			logger.info("Enter 1 to Login");
 			logger.info("Enter 2  to Register (if you are student)");
 			logger.info("Enter any other number to exit CRS");
+			logger.info("==========================================================");
 			choice = Integer.parseInt(sc.nextLine());
 			switch(choice) {
 				case 1:
@@ -61,7 +63,7 @@ public class UserClient {
 					break;
 				default:
 					showMenu = false;
-					logger.error("=====Exiting=====");
+					logger.error("========================EXITING CRS=======================");
 					break;
 			}
 		}while(showMenu);
@@ -116,9 +118,9 @@ public class UserClient {
 		logger.info("Enter Mobile No.");
 		student.setMobile(sc.nextLong());
 		sc.nextLine();
-		logger.info("Enter Branch (Civil, Chem, CS, EEE, Mech, ECE):");
+		logger.info("Enter Branch \n(Civil, Chem, CS, EEE, Mech, ECE):");
 		student.setBranch(Department.valueOf(sc.nextLine()));
-		logger.info("Select Gender: 'M' for male and 'F' for female");
+		logger.info("Select Gender: \n'M' for male and 'F' for female");
 		student.setGender(Gender.valueOf(sc.nextLine()));
 		logger.info("If you have scholarship enter 1, else enter 0");
 		int scholarShip = Integer.parseInt(sc.nextLine());
@@ -138,6 +140,7 @@ public class UserClient {
 			UserDaoImpl temp =  UserDaoImpl.getInstance();
 			int userid=temp.lastEntry();
 			student.setUserId(userid);
+			logger.info("=======================REGISTERED=======================");
 			logger.info("Successfully Registered! Your user id is " + student.getUserId() + ".\nLogging You In");
 			login(student.getUserId(), password);
 		} else {
