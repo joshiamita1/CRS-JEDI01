1e93290e51697541872486ab65c9f44e72d5cae3
abhishreejain <45056175+abhishreejain@users.noreply.github.com>
2021-01-30 21:43:27 +0530

    validate

--- CRS-JEDI-Flipkart/src/com/flipkart/validation/Validate.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/validation/Validate.java
@@ -2,23 +2,36 @@ package com.flipkart.validation;
 
 import java.util.Scanner;
 
-import org.apache.log4j.Logger;
-
 import com.flipkart.exception.InvalidLoginException;
 import com.flipkart.exception.UserNotFoundException;
+
+import client.StudentBusiness;
 
 import org.apache.log4j.Logger;
 
 import com.flipkart.bean.Student;
 import com.flipkart.bean.User;
 import com.flipkart.business.AuthenticateBusiness;
+import com.flipkart.business.ProfessorBusiness;
+import com.flipkart.business.StudentBusiness;
 import com.flipkart.constant.Department;
 import com.flipkart.constant.Role;
 
 public class Validate {
 
+	Scanner sc = new Scanner(System.in);
+	
+	AuthenticateBusiness authenticateBusinessObject = AuthenticateBusiness.getInstance();
+	ProfessorBusiness professorBusinessObject = ProfessorBusiness.getInstance();
+	StudentBusiness studentBusinessObject = StudentBusiness.getInstance();
+	
+	/**
+	 * Validate Admin
+	 * @param user
+	 * @param password
+	 * @returnType void
+	 */
 	public void validateAdmin(User user, String password) {
-		AuthenticateBusiness authenticateBusinessObject = AuthenticateBusiness.getInstance();
 		if(authenticateBusinessObject.registerAdmin(user, password)) {
 			logger.info("Registration Success");
 		} else {
@@ -26,8 +39,14 @@ public class Validate {
 		}
 	}
 	
+	/**
+	 * Validate Professor
+	 * @param user
+	 * @param password
+	 * @param department
+	 * @returnType void
+	 */
 	public void validateProfessor(User user, String password, Department department) {
-		AuthenticateBusiness authenticateBusinessObject = AuthenticateBusiness.getInstance();
 		if(authenticateBusinessObject.registerProfessor(user, password, department)) {
 			logger.info("Registration Success.");
 		} else {
@@ -35,5 +54,81 @@ public class Validate {
 		}
 	}
 	
+	/**
+	 * Validate if Professor teaches a course
+	 * @param professorId
+	 * @param courseId
+	 * @returnType void
+	 */
+	public void validateProfCourse(int professorId, int courseId) {
+		if(professorBusinessObject.validCourseForProfessor(professorId, courseId)) {
+			professorBusinessObject.viewRegisteredStudents(courseId);
+		} else {
+			logger.info("This Course is not taught by you!");
+		}
+	}
 	
+	/**
+	 * Validate the registration of a course for a student
+	 * @param studentId
+	 * @returnType void
+	 */
+	public void validateRegisterCourse(int studentId) {
+		
+		if(studentBusinessObject.numberOfRegisteredCourses(studentId)==4) {
+			logger.info("=========MAXIMUM COURSE REGISTRATION LIMIT REACHED=========");
+			logger.info("You cannot add courses as you have already selected 4 courses");
+		} else{
+			logger.info("Enter course id");
+			int courseId = sc.nextInt();
+			// Check if student is already registered for the course
+			if(studentBusinessObject.checkValidCourseForStudent(studentId, courseId)) {
+				logger.info("====================ALREADY REGISTERED====================");
+				logger.info("You are already Registered for this course");
+			}// Check if there is availability in the course
+			else if(courseCatalogBusinessObject.numberOfRegisterdStudents(courseId)==10) {
+				logger.info("==============MAXIMUM STUDENT LIMIT REACHED==============");
+				logger.info("10 Students already Registerd for the course so you can't register");
+			} else {
+				studentBusinessObject.registerCourse(studentId, courseId);
+			}
+		}
+	}
+	
+	/**
+	 * Validate dropping of a course by a student
+	 * @param studentId
+	 * @param courseId
+	 * @returnType void
+	 */
+	public void validateDropCourse(int studentId, int courseId) {
+		if(studentBusinessObject.checkValidCourseForStudent(studentId, courseId)) {
+			studentBusinessObject.dropCourse(studentId, courseId);
+		} else {
+			logger.info("======================NOT REGISTERED======================");
+			logger.info("You were not registered for this course");
+		}
+	}
+	
+	/**
+	 * Validate fee payment process
+	 * @param studentId
+	 * @param fees
+	 * @returnType void
+	 */
+	public void validateFee(int studentId, double fees) {
+		if(fees==0) {
+			logger.info("No dues pending!");
+		}
+		else{
+			logger.info("Pending Fees = " + fees + "INR-/ only!");
+			logger.info("Please Choose Mode of the Payment\n1. To pay with Credit Card\n2. To pay using cash \n3. To pay using Net Banking");
+			int choice = sc.nextInt();
+			if(choice>=1 && choice <= 3) {
+				studentBusinessObject.makePayment(studentId, fees, choice);
+			} else {
+				logger.info("Invalid Mode.\nTranscation Terminated.");
+			}
+		}
+	}
 }
